98097cc654021f574dd6b8253fb53c27d39dc2dd
eslamyasser <e.yasser997@gmail.com>
2023-06-05 22:05:00 +0300

    Shit

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,14 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
 
+function Products() {
+  const [data, setData] = useState([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://dummyjson.com/products');
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
 
-
- function Products() {
-
-    const [data, setthisData] = useState({})
+    fetchData();
+  }, []);
 
   return (
-    <div>Products</div>
-  )
+    <div>
+
+
+       {data.products.map((product) => (
+        <div key={product.id}>
+          <img src={product.img} alt="its an img" width="30vh" height="40vh" />
+          <h2>{product.name}</h2>
+          <h3>{product.price}</h3>
+          <h4>{product.category}</h4>
+          <p>{product.description}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
-export default Products
+
+export default Products;
